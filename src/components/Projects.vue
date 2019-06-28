b3dde76fac9bbcef30a071e7ef08f9a6eacd3aac
tatoMa <Jason.ma1904@gmail.com>
2019-06-28 17:42:03 +1000

    project section refactor

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,36 +1,91 @@
 <template>
   <v-layout row wrap class="ma-4">
-    <v-flex xs12 sm6 md4 v-for="i in 6" :key="i">
-      <v-card class="ma-3">
-        <v-img
-          :src="require('../assets/img/landing.jpg')"
-          height="200px"
-          class="hidden-xs-and-down"
-        ></v-img>
-        <v-img :src="require('../assets/img/landing.jpg')" height="40px" class="xs-and-down"></v-img>
+    <v-flex xs12 sm6 md4 v-for="(project,index) in projects" :key="index">
+      <v-card class="ma-3" height="300">
+        <v-layout row fill-height>
+          <v-flex xs5>
+            <v-img
+              :src="require(`../assets/img/${project.img}.jpg`)"
+              height="300px"
+              class="hidden-xs-and-down"
+            ></v-img>
+          </v-flex>
+          <v-flex xs7>
+            <v-layout align-center justify-space-between column fill-height>
+              <v-card-title primary-title>
+                <div>
+                  <div class="headline">{{project.title}}</div>
+                  <ul class="mt-2">
+                    <li
+                      class="subheading font-weight-medium"
+                      v-for="(detail,index) in project.details"
+                      :key="index"
+                    >{{ detail }}</li>
+                  </ul>
+                </div>
+              </v-card-title>
 
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Project</div>
-            <span class="grey--text">the web site</span>
-          </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-layout>
-            <v-flex xs6>
-              <v-btn block>Browser</v-btn>
-            </v-flex>
-            <v-flex xs6>
-              <v-btn block>Github</v-btn>
-            </v-flex>
-          </v-layout>
-        </v-card-actions>
+              <v-card-actions>
+                <v-btn outline>Browse</v-btn>
+                <v-btn outline>Github</v-btn>
+              </v-card-actions>
+            </v-layout>
+          </v-flex>
+        </v-layout>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          img: "linker",
+          title: "Linker (Mobile Only)",
+          details: ["Firebase", "Vue", "Balmer"],
+          link: "linker-c26d5.firebaseapp.com",
+          github: "linker-c26d5.firebaseapp.com"
+        },
+        {
+          img: "linker",
+          title: "Linker (Mobile Only)",
+          details: ["Firebase", "Vue", "Balmer"],
+          link: "linker-c26d5.firebaseapp.com",
+          github: "linker-c26d5.firebaseapp.com"
+        },
+        {
+          img: "linker",
+          title: "Linker (Mobile Only)",
+          details: ["Firebase", "Vue", "Balmer"],
+          link: "linker-c26d5.firebaseapp.com",
+          github: "linker-c26d5.firebaseapp.com"
+        },
+        {
+          img: "linker",
+          title: "Linker (Mobile Only)",
+          details: ["Firebase", "Vue", "Balmer"],
+          link: "linker-c26d5.firebaseapp.com",
+          github: "linker-c26d5.firebaseapp.com"
+        },
+        {
+          img: "linker",
+          title: "Linker (Mobile Only)",
+          details: ["Firebase", "Vue", "Balmer"],
+          link: "linker-c26d5.firebaseapp.com",
+          github: "linker-c26d5.firebaseapp.com"
+        },
+        {
+          img: "linker",
+          title: "Linker (Mobile Only)",
+          details: ["Firebase", "Vue", "Balmer"],
+          link: "linker-c26d5.firebaseapp.com",
+          github: "linker-c26d5.firebaseapp.com"
+        }
+      ]
+    };
+  }
+};
 </script>
